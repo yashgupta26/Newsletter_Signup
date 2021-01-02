517a821bb2bc6dbff9ee0f526fa2c9ff8eed0a55
Yash Gupta <hpsyash@gmail.com>
2021-01-03 00:11:27 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,77 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+var https = require("https");
+
+
+const app = express();
+
+app.use(express.static("public"))  //to specifu static files
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us7.api.mailchimp.com/3.0/lists/fe2fd5f7a9";
+
+    const options = {
+        method: "POST",
+        auth: "yash:6c37b94e57c92a8e5acd2c33b28576fa-us7"
+    }
+
+    const request = https.request(url, options, function (response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname+"/success.html")
+
+            
+        }    
+        else {
+            res.sendFile(__dirname+"/failure.html")
+        }
+
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+
+    })
+
+request.write(jsonData);
+request.end();
+
+});
+
+app.post("/failure.html",function(req , res) {
+    res.redirect("/")
+});
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server is running on port 3000");
+});
+
+//api key
+//6c37b94e57c92a8e5acd2c33b28576fa-us7
+// audience id fe2fd5f7a9
